79389048d874042ee56610df50dfae20ca4c1878
Maxim Mamedov <mmd.developer@gmail.com>
2024-07-22 12:25:46 +0700

    Presenter async update

--- Sources/StateMVP/BasePresenter.swift
+++ Sources/StateMVP/BasePresenter.swift
@@ -1,26 +1,36 @@
+import Foundation
+
 open class BasePresenter<S, E> {
-        
     public var stateSignal: ((_ state: S) -> Void)?
     public var eventSignal: ((_ event: E) -> Void)?
     
     public init() { }
-        
+    
     open func loaded() { }
-    
-    open func viewWillAppear() { }
-    
-    open func viewDidAppear() { }
-    
-    open func viewWillDisappear() { }
-    
-    open func viewDidDisappear() { }
-    
+        
     open func updateState(_ state: S) {
-        stateSignal?(state)
+        DispatchQueue.main.async {
+            print("Updating State: \(state)")
+            self.stateSignal?(state)
+        }
     }
     
     open func emitEvent(_ event: E) {
-        eventSignal?(event)
+        DispatchQueue.main.async {
+            print("Updating Event: \(event)")
+            self.eventSignal?(event)
+        }
     }
     
+    @MainActor
+    open func updateMainActorState(_ state: S) {
+        print("Updating Main Actor State: \(state)")
+        stateSignal?(state)
+    }
+    
+    @MainActor
+    open func emitMainActorEvent(_ event: E) {
+        print("Updating Main Actor Event: \(event)")
+        eventSignal?(event)
+    }
 }
